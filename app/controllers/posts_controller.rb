030893da214a6d1557c3d34dfdded06e04eeaebc
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-12-30 08:39:07 +0000

    Listing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.where(publish: true).order(id: :desc).page(params[:page]).per(Setting.post_per_page)
   end
 
   def show
