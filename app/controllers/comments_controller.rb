4624b2c4b0bf190c96088a957e37447c3d4b187a
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-01-02 18:37:07 +0000

    Adding strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
   def create
+  	@visitor = Visitor.new(visitor_comments_params)
+  	@visitor.save
+
+  	redirect_to :back
+  end
+
+  private
+
+  def visitor_comments_params
+  	params.require(:visitor).permit(:fullname, :email, :comments_attributes => [:message, :post_id])
   end
 end
