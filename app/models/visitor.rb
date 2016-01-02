99e93a169e3c4a36a420d3ad25b7f8d02bc6d363
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-01-02 18:37:22 +0000

    Adding accepts nested params

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,4 +2,6 @@ class Visitor < ActiveRecord::Base
 	has_many :notifications, as: :notifiable, dependent: :destroy
 	has_many :comments, dependent: :destroy
 	has_many :messages, dependent: :destroy
+
+	accepts_nested_attributes_for :comments
 end
