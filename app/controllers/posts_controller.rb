e711c351b042643aebc53937fb92d69d00b97fab
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-01-03 13:02:39 +0000

    Used published scope

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     if params[:tag]
       @posts = Post.filter_by_tags(params[:tag]).page(params[:page]).per(Setting.post_per_page)
     else
-      @posts = Post.where(publish: true).order(id: :desc).page(params[:page]).per(Setting.post_per_page)
+      @posts = Post.published.page(params[:page]).per(Setting.post_per_page)
     end
   end
 
