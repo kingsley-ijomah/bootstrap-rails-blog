f49fd903d7237ae32dc40b368ad8dde49093a1dd
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-01-03 13:01:21 +0000

    Use scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :visitor
   has_many :notifications, as: :notifiable, dependent: :destroy
 
+  scope :approved, -> { where status: true }
+
   def self.matching_fullname_or_message params
   	joins(:visitor).where("fullname LIKE ? OR message LIKE ?", "%#{params}%", "%#{params}%")
   end
