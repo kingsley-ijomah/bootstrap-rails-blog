8f11da676b33142d0f0fd679ade5933bb15ce538
kingsley ijomah <kingsley.ijomah@gmail.com>
2016-01-03 13:02:17 +0000

    Adding additional comments to exiting visitor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
-  	@visitor = Visitor.new(visitor_comments_params)
-  	@visitor.save
+  	visitor = Visitor.find_by(email: visitor_comments_params[:email])
+
+  	if visitor
+  		visitor.tap do |v|
+  			v.comments << Comment.new(visitor_comments_params[:comments_attributes]['0'])
+  		end
+  	else
+  		visitor = Visitor.new(visitor_comments_params)
+  	end
+
+  	if visitor.save
+  		flash[:notice] = "Successfully created new comment"
+  	else
+  		flash[:alert] = "There was a problem creating your comment"
+  	end
 
   	redirect_to :back
   end
